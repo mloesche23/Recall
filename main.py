429cd8b703e89f38db459650bfb7abaa4645bfb7
mloesche23 <loeschermichael06@gmail.com>
2025-05-30 20:29:47 -0400

    icons, css file, transitions, design

--- main.py
+++ main.py
@@ -67,3 +67,7 @@ async def getMLBscoresLive():
         games.append(game_data)
 
     return {"games": games}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
